72f7606482fba20f360b2aeafbbaa64f055e8c51
Nora Lashner <nlashner@gmail.com>
2020-06-11 15:18:07 -0400

    can favorite and unfavorite tracks in the queue

--- client/reducer/tracks.js
+++ client/reducer/tracks.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
 
 const GET_TRACKS = 'GET_TRACKS'
+const FAVORITE_TRACK = 'FAVORITE_TRACK'
 
 const getTracks = tracks => {
   return {
     type: GET_TRACKS,
     tracks
+  }
+}
+
+const favoriteTrack = track => {
+  return {
+    type: FAVORITE_TRACK,
+    track
   }
 }
 
@@ -24,7 +32,7 @@ export const favoriteTrackInServer = (id, isFavorite) => {
   return async dispatch => {
     try {
       const {data} = await axios.put('/api/tracks', {id, isFavorite})
-      console.log(data)
+      dispatch(favoriteTrack(data))
     } catch (error) {
       console.error(error)
     }
@@ -42,6 +50,15 @@ export default function tracksReducer(state = initialState, action) {
         ...state,
         tracks: action.tracks
       }
+    case FAVORITE_TRACK: {
+      let trackList = state.tracks
+      let idx = trackList.findIndex(obj => obj.id === action.track.id)
+      trackList[idx] = action.track
+      return {
+        ...state,
+        tracks: trackList
+      }
+    }
     default:
       return state
   }
