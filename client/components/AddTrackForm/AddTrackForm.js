d0bb9eac1d4761ef3224281b84cd225a6be7fe40
Nora Lashner <nlashner@gmail.com>
2020-07-20 12:28:16 -0400

    updated login and signin form to be the same as add track

--- client/components/AddTrackForm/AddTrackForm.js
+++ client/components/AddTrackForm/AddTrackForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {addTrackToServer} from '../../reducer/tracks'
-import './addTrackForm.css'
+import '../form.css'
 
 class AddTrack extends Component {
   constructor() {
--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,34 +2,38 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
+import './form.css'
 
-/**
- * COMPONENT
- */
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div>
+    <div className="form-flex">
       <form onSubmit={handleSubmit} name={name}>
         <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
+          <input
+            name="email"
+            type="text"
+            className="form-input"
+            placeholder="Email"
+          />
         </div>
         <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
+          <input
+            name="password"
+            type="password"
+            className="form-input"
+            placeholder="Password"
+          />
         </div>
         <div>
-          <button type="submit">{displayName}</button>
+          <button type="submit" className="form-button">
+            {displayName}
+          </button>
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
+      {/* <a href="/auth/google">{displayName} with Google</a> */}
     </div>
   )
 }
